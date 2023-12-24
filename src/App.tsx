aeb8ff3f719eaebb2c66252e6d4f362fb6b4bc0c
待看云开 <xyk953651094@qq.com>
2023-12-24 14:19:49 +0800

    简洁模式现在可以隐藏时间了

--- src/App.tsx
+++ src/App.tsx
@@ -219,6 +219,7 @@ function App() {
                         <ClockComponent
                             minorColor={minorColor}
                             holidayData={holidayData}
+                            preferenceData={preferenceData}
                         />
                     </Col>
                     <Col span={24}>
