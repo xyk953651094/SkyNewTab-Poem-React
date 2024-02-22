2c274320796c6a1668b56d7a06908b664f2bd2ba
待看云开 <xyk953651094@qq.com>
2024-02-22 11:55:10 +0800

    提升使用体验

--- src/menuComponents/menuHeaderComponent.tsx
+++ src/menuComponents/menuHeaderComponent.tsx
@@ -13,14 +13,6 @@ function MenuHeaderComponent(props: any) {
             </Col>
             <Col span={12} style={{textAlign: "right"}}>
                 <Space>
-                    <Button type={"text"} shape={props.preferenceData.buttonShape} icon={<GiftOutlined/>}
-                            href={"https://afdian.net/a/xyk953651094"} target={"_self"}
-                            onMouseOver={(e) => btnMouseOver(props.majorColor, e)}
-                            onMouseOut={(e) => btnMouseOut(props.minorColor, e)}
-                            className={"poemFont"}
-                            style={{color: getFontColor(props.minorColor)}}>
-                        捐助
-                    </Button>
                     <Button type={"text"} shape={props.preferenceData.buttonShape} icon={<NotificationOutlined/>}
                             href={"https://xyk953651094.blogspot.com/"} target={"_self"}
                             onMouseOver={(e) => btnMouseOver(props.majorColor, e)}
@@ -28,6 +20,14 @@ function MenuHeaderComponent(props: any) {
                             className={"poemFont"}
                             style={{color: getFontColor(props.minorColor)}}>
                         博客
+                    </Button>
+                    <Button type={"text"} shape={props.preferenceData.buttonShape} icon={<GiftOutlined/>}
+                            href={"https://afdian.net/a/xyk953651094"} target={"_self"}
+                            onMouseOver={(e) => btnMouseOver(props.majorColor, e)}
+                            onMouseOut={(e) => btnMouseOut(props.minorColor, e)}
+                            className={"poemFont"}
+                            style={{color: getFontColor(props.minorColor)}}>
+                        捐助
                     </Button>
                 </Space>
             </Col>
