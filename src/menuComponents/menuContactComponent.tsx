d20c90b9dc08a993f07872366a9ebb109e064021
待看云开 <xyk953651094@qq.com>
2024-02-25 18:37:18 +0800

    提升使用体验

--- src/menuComponents/menuContactComponent.tsx
+++ src/menuComponents/menuContactComponent.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Button, Card, Col, Row} from "antd";
-import {DislikeOutlined, GithubOutlined, GitlabOutlined, LikeOutlined, MailOutlined} from "@ant-design/icons";
+import {DislikeOutlined, GithubOutlined, GitlabOutlined, LikeOutlined, MailOutlined, BulbOutlined} from "@ant-design/icons";
 import {btnMouseOut, btnMouseOver, getFontColor} from "../typescripts/publicFunctions";
 
 function MenuContactComponent(props: any) {
@@ -17,6 +17,24 @@ function MenuContactComponent(props: any) {
               bodyStyle={{backgroundColor: props.minorColor}}
         >
             <Row gutter={[0, 8]}>
+                <Col span="12">
+                    <Button type={"text"} shape={props.preferenceData.buttonShape} icon={<GithubOutlined/>}
+                            href={"https://github.com/xyk953651094/"} target={"_self"}
+                            onMouseOver={(e) => btnMouseOver(props.majorColor, e)}
+                            onMouseOut={(e) => btnMouseOut(props.minorColor, e)}
+                            style={{color: getFontColor(props.minorColor)}} className={"poemFont"}>
+                        作者主页
+                    </Button>
+                </Col>
+                <Col span="12">
+                    <Button type={"text"} shape={props.preferenceData.buttonShape} icon={<GitlabOutlined/>}
+                            href={"https://gitlab.com/xyk953651094/"} target={"_self"}
+                            onMouseOver={(e) => btnMouseOver(props.majorColor, e)}
+                            onMouseOut={(e) => btnMouseOut(props.minorColor, e)}
+                            style={{color: getFontColor(props.minorColor)}} className={"poemFont"}>
+                        作者主页
+                    </Button>
+                </Col>
                 <Col span="12">
                     <Button type={"text"} shape={props.preferenceData.buttonShape} icon={<LikeOutlined/>}
                             href={"mailto:xyk953651094@qq.com?&subject=云开诗词新标签页-功能建议"} target={"_self"}
@@ -36,21 +54,11 @@ function MenuContactComponent(props: any) {
                     </Button>
                 </Col>
                 <Col span="12">
-                    <Button type={"text"} shape={props.preferenceData.buttonShape} icon={<GithubOutlined/>}
-                            href={"https://github.com/xyk953651094/"} target={"_self"}
+                    <Button type={"text"} shape={props.preferenceData.buttonShape} icon={<BulbOutlined />}
                             onMouseOver={(e) => btnMouseOver(props.majorColor, e)}
                             onMouseOut={(e) => btnMouseOut(props.minorColor, e)}
-                            style={{color: getFontColor(props.minorColor)}} className={"poemFont"}>
-                        作者主页
-                    </Button>
-                </Col>
-                <Col span="12">
-                    <Button type={"text"} shape={props.preferenceData.buttonShape} icon={<GitlabOutlined/>}
-                            href={"https://gitlab.com/xyk953651094/"} target={"_self"}
-                            onMouseOver={(e) => btnMouseOver(props.majorColor, e)}
-                            onMouseOut={(e) => btnMouseOut(props.minorColor, e)}
-                            style={{color: getFontColor(props.minorColor)}} className={"poemFont"}>
-                        作者主页
+                            className={"poemFont"} style={{color: getFontColor(props.minorColor)}}>
+                        联系作者前请先查阅帮助文档
                     </Button>
                 </Col>
             </Row>
