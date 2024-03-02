096374d105da7082f072abba83b2bcfde6f71d1f
待看云开 <xyk953651094@qq.com>
2024-03-02 10:54:41 +0800

    提升使用体验

--- src/menuComponents/menuFooterComponent.tsx
+++ src/menuComponents/menuFooterComponent.tsx
@@ -7,9 +7,10 @@ function MenuFooterComponent(props: any) {
     return (
         <Space>
             <Button type={"text"} shape={props.preferenceData.buttonShape} icon={<StarOutlined/>}
+                    href={"https://afdian.net/a/xyk953651094"} target={"_self"}
                     onMouseOver={(e) => btnMouseOver(props.majorColor, e)}
                     onMouseOut={(e) => btnMouseOut(props.minorColor, e)}
-                    className={"poemFont"} style={{color: getFontColor(props.minorColor), cursor: "default"}}>
+                    className={"poemFont"} style={{color: getFontColor(props.minorColor)}}>
                 如果喜欢这款插件，请考虑捐助或五星好评
             </Button>
         </Space>
