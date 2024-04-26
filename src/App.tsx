2347210d43d8171b01739ec50f3f3095fdc790d0
待看云开 <xyk953651094@qq.com>
2024-04-26 13:13:08 +0800

    专注模式新增本地白噪音与专注时长

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,8 @@ function App() {
         let bodyEle = $("body");
         bodyEle.bind("DOMNodeInserted", () => {
             // 通用
-            $(".ant-list-header, .ant-list-item").css({"borderBlockEndColor": getFontColor(minorColor), "padding": "6px 0"});
+            $(".ant-list-header, .ant-list-item").css("borderBlockEndColor", getFontColor(minorColor));
+            $(".ant-list-header, .ant-list-item, .ant-list-footer").css("padding", "6px 0");
             $(".ant-list-item-meta-title").css("color", getFontColor(minorColor));
             $(".ant-list-item-meta-description").css("color", getFontColor(minorColor));
             $(".ant-list-item-action").css("marginInlineStart", "0");
