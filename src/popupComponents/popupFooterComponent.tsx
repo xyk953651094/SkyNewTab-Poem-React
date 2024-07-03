49d717a3e68d7cc03d3b96abb0bf12f59f1fb965
待看云开 <xyk953651094@qq.com>
2024-07-03 09:28:44 +0800

    提升使用体验

--- src/popupComponents/popupFooterComponent.tsx
+++ src/popupComponents/popupFooterComponent.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Button, Space} from "antd";
-import {GiftOutlined, GithubOutlined, GitlabOutlined, WechatOutlined} from "@ant-design/icons";
+import {GithubOutlined, GitlabOutlined, InfoCircleOutlined} from "@ant-design/icons";
 import {btnMouseOut, btnMouseOver, getFontColor} from "../typescripts/publicFunctions";
 
 function PopupFooterComponent(props: any) {
@@ -20,19 +20,11 @@ function PopupFooterComponent(props: any) {
                     style={{color: getFontColor(props.majorColor)}} className={"popupFont"}>
                 产品主页
             </Button>
-            <Button type={"text"} shape={props.preferenceData.buttonShape} icon={<WechatOutlined />}
-                    href={"https://github.com/xyk953651094/xyk953651094/assets/28004442/fd605f5c-d2ca-43eb-ae16-86d17d5f6fb1/"} target={"_blank"}
+            <Button type={"text"} shape={props.preferenceData.buttonShape} icon={<InfoCircleOutlined/>}
                     onMouseOver={(e) => btnMouseOver(props.minorColor, e)}
                     onMouseOut={(e) => btnMouseOut(props.majorColor, e)}
-                    style={{color: getFontColor(props.majorColor)}} className={"popupFont"}>
-                公众号
-            </Button>
-            <Button type={"text"} shape={props.preferenceData.buttonShape} icon={<i className="bi bi-rss-fill"></i>}
-                    href={"https://xyk953651094.blogspot.com/"} target={"_blank"}
-                    onMouseOver={(e) => btnMouseOver(props.minorColor, e)}
-                    onMouseOut={(e) => btnMouseOut(props.majorColor, e)}
-                    style={{color: getFontColor(props.majorColor)}} className={"popupFont"}>
-                博客
+                    style={{color: getFontColor(props.majorColor), cursor: "default"}} className={"popupFont"}>
+                {"版本：V" + require('../../package.json').version}
             </Button>
         </Space>
     );
